29c81480ca241fb7c78e7d8cfe04c061f72c0b10
Victor Osibajo <victoroshi@gmail.com>
2023-10-19 02:11:49 +0000

    ediitng and rewriting printf function

--- _printf.c
+++ _printf.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 /**
- * _printf - function declaration string and variable argument.
- * @format: var argv specifies
- * Return: length of the string.
+ * _printf - function that produces output according to a format
+ * @format: variable arguments 
+ * Return: lenght of the string specifier.
  */
 int _printf(const char * const format, ...)
 {
@@ -47,74 +45,3 @@ Here:
 	return (len);
 }
 
-=======
-=======
->>>>>>> 02e1d858b12567b37e7558c71e85b6b2e636a12b
-void print_buffer(char buffer[], int *buff_ind);
-
-/**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
-int _printf(const char *format, ...)
-{
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
-<<<<<<< HEAD
->>>>>>> 02e1d858b12567b37e7558c71e85b6b2e636a12b
-=======
->>>>>>> 02e1d858b12567b37e7558c71e85b6b2e636a12b
