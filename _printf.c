6080ef64589ba0924e712545e07617642c929c84
Victor Osibajo <victoroshi@gmail.com>
2023-10-21 06:58:40 +0100

    removing unnecessary update

--- _printf.c
+++ _printf.c
@@ -1,49 +1,46 @@
 #include "main.h"
-/**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
- */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	int i, unknow_len = 0, printed_chars = -1;
-	fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
-	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[i].fmt == '\0')
-	{
-		if (fmt[*ind] == '\0')
-			return (-1);
-		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
-		else if (width)
-		{
-			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
-			return (1);
-		}
-		unknow_len += write(1, &fmt[*ind], 1);
-		return (unknow_len);
-	}
-	return (printed_chars);
+/**
+ * _printf - function that produces output according to a format
+ * @format: variable arguments
+ * Return: lenght of the string specifier.
+ */
+int _printf(const char * const format, ...)
+{
+        convert_match m[] = {
+                {"%s", print_string}, {"%c", print_char},
+                {"%%", print_37},
+                {"%i", print_int}, {"%d", print_dec}, {"%r", print_srev},
+                {"%R", print_rot13}, {"%b", print_bi}, {"%u", print_unsigned},
+                {"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
+                {"%S", print_exclusive_string}, {"%p", print_pointer}
+        };
+
+        va_list args;
+        int i = 0, j, len = 0;
+
+        va_start(args, format);
+        if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+                return (-1);
+
+Here:
+        while (format[i] != '\0')
+        {
+                j = 13;
+                while (j >= 0)
+                {
+                        if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+                        {
+                                len += m[j].f(args);
+                                i = i + 2;
+                                goto Here;
+                        }
+                        j--;
+                }
+                _putchar(format[i]);
+                len++;
+                i++;
+        }
+        va_end(args);
+        return (len);
 }
